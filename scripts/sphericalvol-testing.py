545e0c366e1620a8ed843f216c90f309f417e414
Patrick Adams <patrick_aust@hotmail.com>
2021-03-31 11:55:45 +1100

    demo difference between filter and convole blur in sphericalvol-testing.py

--- scripts/sphericalvol-testing.py
+++ scripts/sphericalvol-testing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 plt.close('all')
 
-n_angle = 360
+n_angle = 180
 grid_type = 'DH2'
 # grid_type = 'GLQ'
 extend=False
@@ -16,20 +16,49 @@ cif = scorpy.CifData('../data/xtal/1al1-sf.cif')
 sphvol = scorpy.SphericalVol(256, n_angle,qmax=cif.qmax, grid_type=grid_type, extend=extend)
 
 sphvol.fill_from_cif(cif)
+sphvol2 = sphvol.copy()
 
-sphvol.plot_slice(index=91)
-plt.title('Initial')
+
+
+i = 81
+
+plt.figure()
+
+plt.subplot(231)
+sphvol.plot_slice(index=i-1, new_fig=False)
+plt.title(f'Initial (nq={i-1})')
+plt.colorbar()
+
+plt.subplot(232)
+sphvol.plot_slice(index=i, new_fig=False)
+plt.title(f'Initial (nq={i})')
+plt.colorbar()
+
+plt.subplot(233)
+sphvol.plot_slice(index=i+1, new_fig=False)
+plt.title(f'Initial (nq={i+1})')
+plt.colorbar()
+
+
+# sphvol.convolve(kern_n=9, std_x=0.25, std_y=0.25 )
 
 sphvol.pass_filter()
 
-sphvol.plot_slice(index=91)
-plt.title('Filtered')
+plt.subplot(234)
+sphvol.plot_slice(index=i-1, new_fig=False)
+plt.title(f'Filtered (nq={i-1})')
+plt.colorbar()
 
-# sphvol.rotate(2*np.pi/3,np.pi/4,np.pi/7)
-sphvol.rotate(0,np.pi/6,0)
+plt.subplot(235)
+sphvol.plot_slice(index=i, new_fig=False)
+plt.title(f'Filtered (nq={i})')
+plt.colorbar()
 
-sphvol.plot_slice(index=91)
-plt.title('Rotated')
+plt.subplot(236)
+sphvol.plot_slice(index=i+1, new_fig=False)
+plt.title(f'Filtered (nq={i+1})')
+plt.colorbar()
+
 
 
 
