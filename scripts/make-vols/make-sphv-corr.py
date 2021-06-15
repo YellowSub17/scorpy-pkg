a59ad3c92221b46d7097114f36fb191b8d221db2
Patrick Adams <patrick_aust@hotmail.com>
2021-06-15 19:57:29 +1000

    working diagonal corrleation with sphv

--- scripts/make-vols/make-sphv-corr.py
+++ scripts/make-vols/make-sphv-corr.py
@@ -27,7 +27,7 @@ pp, tt = np.meshgrid(sphv.phipts, sphv.thetapts)
 #for every pair of q1 and q2 shells...
 for q1_ind in range(0, sphv.nq):
     q1_slice = sphv.vol[q1_ind, ...]
-    for q2_ind in range(0, sphv.nq):
+    for q2_ind in range(q1_ind, sphv.nq):
         q2_slice = sphv.vol[q2_ind, ...]
 
 
@@ -51,11 +51,22 @@ for q1_ind in range(0, sphv.nq):
                 angle_between_rolled = np.array(angle_between_ind).reshape(sphv.ntheta, sphv.nphi)
 
                 #correlate the intensities, where q2 is oritented
-                II = q1_slice * np.roll(q2_slice, (theta_ind, phi_ind), (0, 1)) #* np.sin(tt_rolled) * np.sin(tt)
+                II1 = q1_slice * np.roll(q2_slice, (theta_ind, phi_ind), (0, 1)) #* np.sin(tt_rolled) * np.sin(tt)
+                II2 = q2_slice * np.roll(q1_slice, (theta_ind, phi_ind), (0, 1)) #* np.sin(tt_rolled) * np.sin(tt)
 
                 #for every pair of psi index and inntensity values, add them to the correlation volume
-                for angle_ind, II_val in zip(angle_between_rolled.flatten(), II.flatten()):
-                    corr.vol[q1_ind, q2_ind, angle_ind] += II_val
+
+                if q1_ind == q2_ind:
+                    for angle_ind, II_val in zip(angle_between_rolled.flatten(), II1.flatten()):
+                        corr.vol[q1_ind, q2_ind, angle_ind] += II_val
+
+                else:
+                    for angle_ind, II_val in zip(angle_between_rolled.flatten(), II1.flatten()):
+                        corr.vol[q1_ind, q2_ind, angle_ind] += II_val
+
+                    for angle_ind, II_val in zip(angle_between_rolled.flatten(), II2.flatten()):
+                        corr.vol[q2_ind, q1_ind, angle_ind] += II_val
+
 
 
 
