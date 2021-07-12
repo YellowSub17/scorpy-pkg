9c146fd38a4de599979facaee30868d7b4798c9c
Patrick Adams <patrick_aust@hotmail.com>
2021-07-12 16:58:55 +1000

    improved pattern sim scratch

--- scripts/crystfel/h5totxt.py
+++ scripts/crystfel/h5totxt.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 '''
-hdf5_2_espk.py
+h5totxt.py
 
 Convert a H5 generated from pattern_sim to an espk peaklist
+
+Usage:
+    h5totxt [infpath] [outfpath] [ncrystals]
 '''
+
+
 
 import sys
 import scorpy
@@ -13,8 +18,13 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
-# infname = sys.argv[1]
-# outfname = sys.argv[2]
+
+
+infpath = sys.argv[1]
+outfpath = sys.argv[2]
+ncrystals = sys.argv[3]
+
+
 
 infname = f'{__DATADIR}/out.h5'
 outfname = f'{__DATADIR}/out.txt'
@@ -22,15 +32,11 @@ outfname = f'{__DATADIR}/out.txt'
 
 
 
-print(infname)
 
-f = h5py.File(infname, 'r')
+h5f = h5py.File(infname, 'r')
 
-data = f['entry_1/instrument_1/detector_1/data'][:]
+data = h5f['entry_1/instrument_1/detector_1/data'][:]
 
-if np.all(data==0):
-    print('crash')
-    sys.exit()
 
 
 
@@ -50,7 +56,19 @@ f.close()
 
 
 geo = scorpy.ExpGeom(f'{__DATADIR}/geoms/agipd_2304_vj_opt_v3.geom')
-pk = scorpy.PeakData(outfname, geo, cxi_flag=False)
+# pk = scorpy.PeakData(outfname, geo, cxi_flag=False)
+
+# plt.figure()
+# ax = plt.axes()
+# ax.set_facecolor('#000000')
+# geo.plot_panels()
+# pk.plot_peaks(cmap='summer')
+# plt.colorbar()
+
+
+
+
+pk = scorpy.PeakData(f'{scorpy.env.__DATADIR}/out.h5', geo)
 
 plt.figure()
 ax = plt.axes()
@@ -59,12 +77,8 @@ geo.plot_panels()
 pk.plot_peaks(cmap='summer')
 plt.colorbar()
 
+
+
 plt.show()
-
-
-
-
-
-
 
 
--- scripts/crystfel/scratch.sh
+++ scripts/crystfel/scratch.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
 
+
+
 DATA_PATH='/home/pat/Documents/cloudstor/phd/python_projects/scorpy-pkg/data'
 
 ## Experiment Params
@@ -7,16 +9,30 @@ GEOM="${DATA_PATH}/geoms/agipd_2304_vj_opt_v3.geom"
 BEAM_RADIUS="1e-6"
 NUM_PHOTONS="1e12"
 PHOTON_E="9300"
-#PHOTON_E="18600"
-BEAM_BANDWIDTH="0.01"
+BEAM_BANDWIDTH="0.001"
 SPECTRUM_SHAPE="tophat"
 NUM_SAMPLE_SPEC="1"
+BACKGROUND="0"
 
 
 ## Crystal Params
 PDB="${DATA_PATH}/xtal/1al1.pdb"
 NUM_CRYSTALS="1"
-SIZE="900"
+SIZE="10"
+
+
+## Calc Params
+GRAD='mosaic'
+TAG="out"
+
+
+
+
+if [ ${NUM_CRYSTALS} == "1" ]; then
+    TAG="${TAG}.h5"
+fi
+
+
 
 
 
@@ -29,7 +45,10 @@ pattern_sim \
     --random-orientation \
     --really-random \
     --no-fringes \
+    --no-noise \
+    --flat \
     -n ${NUM_CRYSTALS} \
+    --background=${BACKGROUND} \
     --max-size=${SIZE} \
     --min-size=${SIZE} \
     --beam-radius=${BEAM_RADIUS} \
@@ -38,9 +57,35 @@ pattern_sim \
     --photon-energy=${PHOTON_E} \
     --spectrum=${SPECTRUM_SHAPE} \
     --sample-spectrum ${NUM_SAMPLE_SPEC} \
+    --gradients=${GRAD} \
     -g ${GEOM} \
     -p ${PDB} \
-    -o ${DATA_PATH}/out.h5
+    -o ${DATA_PATH}/${TAG} 
+
+
+
+
+
+for f in $(ls ${DATA_PATH}/${TAG}*); do
+    h5ls -d ${f}/entry_1/instrument_1/detector_1/data | grep -m 1 -q -e '\.'
+    if  [ $? -eq 0 ]
+    then
+        echo "${f} has Intensity"
+    else
+        echo "${f} has no Intensity"
+        rm ${f}
+    fi
+
+done
+
+
+
+
+
+
+
+
+
 
 
 
