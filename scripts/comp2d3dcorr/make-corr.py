bdfe0826bdab423fecd4f1623a8adc9245449208
Patrick Adams <patrick_aust@hotmail.com>
2022-03-01 17:44:00 +1100

    rm batch-2d-corr to make-corr

--- scripts/comp2d3dcorr/make-corr.py
+++ scripts/comp2d3dcorr/make-corr.py
@@ -3,6 +3,8 @@ import numpy as np
 import scorpy
 import matplotlib.pyplot as plt
 plt.close('all')
+import os
+import time
 
 
 
@@ -24,8 +26,128 @@ corr.fill_from_cif(cif, verbose=2, method='scat_sph')
 
 corr.save(f'{scorpy.DATADIR}/dbins/inten1-qmax0264-3d-sph-noselfcorr-qcor.dbin')
 
-corr.plot_q1q2()
-corr.plot_q1q2(log=True)
+
+
+
+
+######2D CORR
+
+
+## parameters
+
+size = 75
+photonenergy = 9300
+qmax=0.264
+qmin=0.01
+clen = 0.45
+npix = 250
+pixsize = 800e-6
+nphotons=1e24
+nofringes=True
+integration_r = 0.005
+pdbfname = 'inten1-qmax1.pdb'
+intenfname = 'inten1-qmax1-sf.hkl'
+
+
+
+
+geomfname = 'batch.geom'
+nbatches = 100
+nframes = 100
+method = 'scat_sph'
+
+corrfname = 'inten1-qmax0264-2d-ssph-batch-int-noselfcorr-qcor.dbin'
+
+
+
+corr = scorpy.CorrelationVol(nq=100, npsi=180, qmax=qmax, cos_sample=False, inc_self_corr=False)
+
+
+#write geom
+geomf = open(f'{scorpy.DATADIR}/geoms/{geomfname}', 'w')
+geomf.write('data = /entry_1/instrument_1/detector_1/data\n')
+geomf.write(f'mask = /entry_1/instrument_1/detector_1/mask\n')
+geomf.write(f'adu_per_eV = 0.0075\n')
+geomf.write(f'clen = {clen}\n')
+geomf.write(f'photon_energy = {photonenergy}\n')
+geomf.write(f'dim0 = %\n')
+geomf.write(f'dim1 = ss\n')
+geomf.write(f'dim2 = fs\n')
+geomf.write(f'res = {int(1/pixsize)}\n')
+geomf.write(f'p0/min_fs = 0\n')
+geomf.write(f'p0/min_ss = 0\n')
+geomf.write(f'p0/max_fs = {npix}\n')
+geomf.write(f'p0/max_ss = {npix}\n')
+geomf.write(f'p0/corner_x = -{int(npix/2)}\n')
+geomf.write(f'p0/corner_y = -{int(npix/2)}\n')
+geomf.write(f'p0/coffset = 0\n')
+geomf.write(f'p0/fs = +0.0x +1.0y\n')
+geomf.write(f'p0/ss = +1.0x +0.0y\n')
+geomf.write(f'nfs = {npix}\n')
+geomf.write(f'nss = {npix}\n')
+geomf.close()
+
+
+geo = scorpy.ExpGeom(f'{scorpy.DATADIR}/geoms/{geomfname}')
+
+
+cmd = 'pattern_sim '
+cmd+='--gpu '
+cmd+=f'-n {nframes} '
+cmd+=f'--max-size={size} '
+cmd+=f'--min-size={size} '
+cmd+=f'--nphotons={nphotons} '
+cmd+=f'--photon-energy={photonenergy} '
+cmd+=f'--intensities={scorpy.DATADIR}/xtal/{intenfname} '
+if nofringes:
+    cmd+=f'--no-fringes '
+cmd+=f'--spectrum=tophat '
+cmd+=f'--sample-spectrum=1 '
+cmd+=f'--gradients=mosaic '
+cmd+=f'-g {scorpy.DATADIR}/geoms/{geomfname} '
+cmd+=f'-p {scorpy.DATADIR}/xtal/{pdbfname} '
+
+if nframes>1:
+    cmd+=f'-o /tmp/corrbatch '
+else:
+    cmd+=f'-o /tmp/corrbatch-1.h5 '
+
+cmd+='--random-orientation '
+cmd+='--really-random '
+
+
+
+
+
+print('############')
+print(f'Correlation started: {time.asctime()}\n')
+for batch in range(nbatches):
+
+    print('batch:', batch+1, end='\r')
+    for file in os.listdir('/tmp/'):
+        if 'corrbatch' in file:
+            os.remove(f'/tmp/{file}')
+
+    os.system(f'{cmd} >/dev/null 2>&1')
+
+    for frame in range(1, nframes+1):
+
+        # print('frame:', frame, end='\r')
+
+        pk = scorpy.PeakData(f'/tmp/corrbatch-{frame}.h5', geo=geo, qmax=qmax, qmin=qmin)
+        pk = pk.integrate_peaks(integration_r)
+        corr.fill_from_peakdata(pk, method=method, verbose=0)
+
+    # print(99*' ', end='')
+
+
+
+    corr.save(f'{scorpy.DATADIR}/dbins/{corrfname}')
+
+
+print(f'Correlation finished: {time.asctime()}\n')
+print('############')
+corr.save(f'{scorpy.DATADIR}/dbins/{corrfname}')
 
 
 
@@ -34,4 +156,3 @@ corr.plot_q1q2(log=True)
 
 
 
-plt.show()
--- scripts/comp2d3dcorr/selfcorr-test.py
+++ scripts/comp2d3dcorr/selfcorr-test.py
@@ -9,22 +9,37 @@ plt.close('all')
 
 
 
-method = 'scat_rect'
+# method = 'scat_sph'
 
-cif = scorpy.CifData(path=f'{scorpy.DATADIR}/xtal/inten1-qmax1-sf.cif', qmax=0.264)
+# cif = scorpy.CifData(path=f'{scorpy.DATADIR}/xtal/inten1-qmax1-sf.cif', qmax=0.264)
+
+# corr1 = scorpy.CorrelationVol(100, 180, 0.264, cos_sample=False, inc_self_corr=True)
+# corr1.fill_from_cif(cif, method=method)
+# corr2 = scorpy.CorrelationVol(100, 180, 0.264, cos_sample=False, inc_self_corr=False)
+# corr2.fill_from_cif(cif, method=method)
+
+# corr1.plot_q1q2()
+# corr2.plot_q1q2()
+
+method= 'scat_pol'
+
+geom = scorpy.ExpGeom(f'{scorpy.DATADIR}/geoms/plot-test.geom')
+pk = scorpy.PeakData(f'{scorpy.DATADIR}/patternsim/plot-test.h5', geo=geom)
+pk.plot_peaks()
 
 
 
-corr1 = scorpy.CorrelationVol(100, 180, 0.264, cos_sample=False, inc_self_corr=True)
-corr1.fill_from_cif(cif, method=method)
+corr1 = scorpy.CorrelationVol(100, 180, 1.48, cos_sample=False, inc_self_corr=True)
+corr1.fill_from_peakdata(pk, method=method)
 
-corr2 = scorpy.CorrelationVol(100, 180, 0.264, cos_sample=False, inc_self_corr=False)
-corr2.fill_from_cif(cif, method=method)
-
+corr2 = scorpy.CorrelationVol(100, 180, 1.48, cos_sample=False, inc_self_corr=False)
+corr2.fill_from_peakdata(pk, method=method)
 
 
 corr1.plot_q1q2()
 corr2.plot_q1q2()
+
+
 
 
 plt.show()
