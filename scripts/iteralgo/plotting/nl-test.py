4a3580f921ff953e32764d13376c147aac2308ed
Patrick Adams <patrick_aust@hotmail.com>
2022-07-25 15:26:48 +1000

    commit before tm tags_subtags in post-recon.py

--- scripts/iteralgo/plotting/nl-test.py
+++ scripts/iteralgo/plotting/nl-test.py
@@ -22,9 +22,9 @@ for i, nl in enumerate(nls):
     print(nl)
 
     a = scorpy.AlgoHandler(f'agno3-nl{nl}')
-    # a.plot_mean_dxyzs('a',fig=fig, axes=axes, color = cmap[i], label=f'{nl}',dx=0.1*i, title='mean atomic displacement')
-    a.plot_vs_count('b', 'std_ddistances',marker='.',fig=fig, axes=axes, color = cmap[i], label=f'{nl}')
-    a.plot_vs_count('b', 'max_ddistances',marker='x',fig=fig, axes=axes, color = cmap[i], label=f'{nl}')
+    a.plot_vs_count('b', 'rfs', marker='.',fig=fig, axes=axes, color = cmap[i], label=f'{nl}', ylabel='$R_f$', xlabel='Iteration')
+    # a.plot_vs_count('b', 'std_ddistances',marker='.',fig=fig, axes=axes, color = cmap[i], label=f'{nl}')
+    # a.plot_vs_count('b', 'max_ddistances',marker='x',fig=fig, axes=axes, color = cmap[i], label=f'{nl}')
 
 
 
