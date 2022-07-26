d3c86eb2f2e72e239b9f489e755b9c494f6fa82a
Patrick Adams <patrick_aust@hotmail.com>
2022-07-26 20:39:54 +1000

    commit before fixing post recon

--- scripts/iteralgo/post-recon.py
+++ scripts/iteralgo/post-recon.py
@@ -13,72 +13,72 @@ plt.close('all')
 
 
 
-#### recipe study
-counts = [i for i in range(121)]
+# #### recipe study
+# counts = [i for i in range(121)]
 
 
-subtags = ['ER120', 
-            'HIO10ER10',
-            'HIO20ER20',
-            'HIO30ER30',
-            'HIO60ER60',
-            'HIO120',
-           ]
-
-
-
-
-
-
-for subtag in subtags:
-    tag = f'agno3-rec'
-    print(f'##### {tag}')
-    a = scorpy.AlgoHandler(tag)
-#     a.run_shelx(subtag)
-    # a.run_shelx(subtag, count='targ')
-    # for count in counts:
-        # print(f'shelxl {count}', end='\r')
-        # a.run_shelx(subtag, count=count)
-
-    a.save_rfs(subtag, verbose=99)
-    a.save_dgeom(subtag, geometry='distances', verbose=99)
-    a.save_dgeom(subtag, geometry='angles', verbose=99)
-    a.save_dxyzs(subtag, verbose=99)
-
-    print()
-    print()
+# subtags = ['ER120', 
+            # 'HIO10ER10',
+            # 'HIO20ER20',
+            # 'HIO30ER30',
+            # 'HIO60ER60',
+            # 'HIO120',
+           # ]
 
 
 
 
 
 
+# for subtag in subtags:
+    # tag = f'agno3-rec'
+    # print(f'##### {tag}')
+    # a = scorpy.AlgoHandler(tag)
+# #     a.run_shelx(subtag)
+    # # a.run_shelx(subtag, count='targ')
+    # # for count in counts:
+        # # print(f'shelxl {count}', end='\r')
+        # # a.run_shelx(subtag, count=count)
+
+    # a.save_rfs(subtag, verbose=99)
+    # a.save_dgeom(subtag, geometry='distances', verbose=99)
+    # a.save_dgeom(subtag, geometry='angles', verbose=99)
+    # a.save_dxyzs(subtag, verbose=99)
+
+    # print()
+    # print()
 
 
 
-#### NL study
-nls = [i for i in range(60, 181, 15)]
-counts = [i for i in range(111)]
-subtags = ['a','b','c','d']
 
-for subtag in subtags:
-    for nl in nls:
-        tag = f'agno3-nl{nl}'
-        print(f'##### {tag}')
-        a = scorpy.AlgoHandler(tag)
-#         a.run_shelx(subtag)
-        # a.run_shelx(subtag, count='targ')
-        # for count in counts:
-            # print(f'shelxl {count}', end='\r')
-            # a.run_shelx(subtag, count=count)
 
-        a.save_rfs(subtag, verbose=99)
-        a.save_dgeom(subtag, geometry='distances', verbose=99)
-        a.save_dgeom(subtag, geometry='angles', verbose=99)
-        a.save_dxyzs(subtag, verbose=99)
 
-    print()
-    print()
+
+
+
+# #### NL study
+# nls = [i for i in range(60, 181, 15)]
+# counts = [i for i in range(111)]
+# subtags = ['a','b','c','d']
+
+# for subtag in subtags:
+    # for nl in nls:
+        # tag = f'agno3-nl{nl}'
+        # print(f'##### {tag}')
+        # a = scorpy.AlgoHandler(tag)
+# #         a.run_shelx(subtag)
+        # # a.run_shelx(subtag, count='targ')
+        # # for count in counts:
+            # # print(f'shelxl {count}', end='\r')
+            # # a.run_shelx(subtag, count=count)
+
+        # a.save_rfs(subtag, verbose=99)
+        # a.save_dgeom(subtag, geometry='distances', verbose=99)
+        # a.save_dgeom(subtag, geometry='angles', verbose=99)
+        # a.save_dxyzs(subtag, verbose=99)
+
+    # print()
+    # print()
 
 
 
