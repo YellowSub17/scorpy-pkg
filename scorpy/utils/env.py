7868f3886ef82c356ac673e6d76203ba31a844b5
Patrick Adams <patrick_aust@hotmail.com>
2022-10-20 15:08:34 +1100

    updated data path;

--- scorpy/utils/env.py
+++ scorpy/utils/env.py
@@ -10,7 +10,9 @@ if sys.platform =='linux':
 
     SCORPYDIR = Path('/home/pat/Documents/cloudstor/phd/python_projects/scorpy-pkg/')
     PADFDIR = Path('/home/pat/Documents/cloudstor/phd/python_projects/padf/')
-    DATADIR = SCORPYDIR / 'data'
+    # DATADIR = SCORPYDIR / 'data'
+    DATADIR = Path('/media/pat/datadrive/scorpy-data')
+
 
 elif sys.platform =='win32':
     SCORPYDIR = Path('C:\\Users\\s3826109\\Documents\\scorpy-pkg')
