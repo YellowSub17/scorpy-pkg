001dd57ac4c9d1f7f5ef397a1cd6405895fff24f
Patrick Adams <patrick_aust@hotmail.com>
2023-03-06 17:38:24 +1100

    commit before clean up iteralgo plots

--- scripts/ice/scratch.py
+++ scripts/ice/scratch.py
@@ -11,24 +11,32 @@ plt.close('all')
 
 
 
-corra = scorpy.CorrelationVol(100, 180, 2.35, cos_sample=False)
-corrb = scorpy.CorrelationVol(100, 180, 2.35, cos_sample=False)
+# corra = scorpy.CorrelationVol(100, 180, 2.35, cos_sample=False)
+# corrb = scorpy.CorrelationVol(100, 180, 2.35, cos_sample=False)
 
 
-for i in range(1, 3721):
-    print(i)
-    corr = scorpy.CorrelationVol(path=f'{scorpy.DATADIR}/ice/sim/corr/hex-ice-1um-{i}-qcor.npy')
-    if i%2==0:
-        corra.vol +=corr.vol
-    else:
-        corrb.vol +=corr.vol
+# for i in range(1, 3721):
+    # print(i)
+    # corr = scorpy.CorrelationVol(path=f'{scorpy.DATADIR}/ice/sim/corr/hex-ice-1um-{i}-qcor.npy')
+    # if i%2==0:
+        # corra.vol +=corr.vol
+    # else:
+        # corrb.vol +=corr.vol
 
 
 
 
+# corra.save(f'{scorpy.DATADIR}/ice/sim/corr/hex-ice-1um-a-qcor.npy')
+# corrb.save(f'{scorpy.DATADIR}/ice/sim/corr/hex-ice-1um-b-qcor.npy')
 
-corra.plot_q1q2()
-corrb.plot_q1q2()
+
+
+corra = scorpy.CorrelationVol(path=f'{scorpy.DATADIR}/ice/sim/corr/hex-ice-1um-a-qcor.npy')
+corrb = scorpy.CorrelationVol(path=f'{scorpy.DATADIR}/ice/sim/corr/hex-ice-1um-b-qcor.npy')
+
+
+corra.plot_q1q2(vminmax=(0, 1e9), title='corra')
+corrb.plot_q1q2(vminmax=(0, 1e9), title='corrb')
 plt.show()
 
 
