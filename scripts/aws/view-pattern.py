68fcba5f62f12067af97619013f3c296e117095c
YellowSub17 <patrick_aust@hotmail.com>
2023-10-09 03:49:02 +0000

    commit from aws

--- scripts/aws/view-pattern.py
+++ scripts/aws/view-pattern.py
@@ -10,64 +10,20 @@ q_inte_r = min(cif.ast_mag, cif.bst_mag, cif.cst_mag)
 
 
 pdb_code ='193l'
-xtal_size = '80nm'
+xtal_size = '100nm'
+geom_code = f'19MPz040'
 
-super_chunk = ''
+super_chunk = 'x0'
 chunk=0
 frame = 3
 
 ns  = 1
 
-# for bw in ['10', '01', '001', '0']:
-    # geom_code = f'19MPz040bw{bw}'
-    # fig, axes = plt.subplots(1,2, sharex=True, sharey=True)
-    # plt.title(f'{xtal_size} orig')
-
-    # npz_fname =  f'/home/ec2-user/corr/data/frames/bw-tests/{pdb_code}-{xtal_size}-{geom_code}-ns{ns}-{frame}.npz'
-    # pk = scorpy.PeakData(npz_fname, f'/home/ec2-user/corr/data/geom/{geom_code}.geom')
-
-    # r_inte_r = pk.convert_q2r(q_inte_r)/2
-
-    # pk.plot_peaks(fig=fig, ax=axes[0])
-    # pk.plot_peakr(r_inte_r, fig=fig, ax=axes[0])
-
-    # inte = pk.integrate_peaks(r_inte_r)
-    # pk.calc_scat(inte[:,0:3], inte[:,-1])
-
-    # pk.plot_peaks(fig=fig, ax=axes[1])
-    # plt.title(f'bw {bw}')
-    # pk.plot_qring(0.4)
-
-
-# for frame in [0, 1, 2, 3, 4,]:
-    # geom_code = f'19MPz040bw001'
-    # fig, axes = plt.subplots(1,2, sharex=True, sharey=True)
-    # plt.title(f'{xtal_size} orig')
-
-    # npz_fname =  f'/home/ec2-user/corr/data/frames/bw-tests/{pdb_code}-{xtal_size}-{geom_code}-ns{ns}-{frame}.npz'
-    # pk = scorpy.PeakData(npz_fname, f'/home/ec2-user/corr/data/geom/{geom_code}.geom')
-
-    # r_inte_r = pk.convert_q2r(q_inte_r)/2
-
-    # pk.plot_peaks(fig=fig, ax=axes[0])
-    # pk.plot_peakr(r_inte_r, fig=fig, ax=axes[0])
-
-    # inte = pk.integrate_peaks(r_inte_r)
-    # pk.calc_scat(inte[:,0:3], inte[:,-1])
-
-    # pk.plot_peaks(fig=fig, ax=axes[1])
-    # plt.title(f'frame {frame}')
-    # pk.plot_qring(0.4)
-
-
-
-frame = 0
-for ns in [1, 3, 5, 8, 16, 32]:
-    geom_code = f'19MPz040bw001'
+for frame in [0, 1, 2, 3, 4,]:
     fig, axes = plt.subplots(1,2, sharex=True, sharey=True)
     plt.title(f'{xtal_size} orig')
 
-    npz_fname =  f'/home/ec2-user/corr/data/frames/bw-tests/{pdb_code}-{xtal_size}-{geom_code}-ns{ns}-{frame}.npz'
+    npz_fname =  f'/home/ec2-user/corr/data/frames/{xtal_size}-{geom_code}-{super_chunk}/{chunk}/{pdb_code}-{xtal_size}-{geom_code}-{super_chunk}-{chunk}-{frame}.npz'
     pk = scorpy.PeakData(npz_fname, f'/home/ec2-user/corr/data/geom/{geom_code}.geom')
 
     r_inte_r = pk.convert_q2r(q_inte_r)/2
@@ -79,10 +35,8 @@ for ns in [1, 3, 5, 8, 16, 32]:
     pk.calc_scat(inte[:,0:3], inte[:,-1])
 
     pk.plot_peaks(fig=fig, ax=axes[1])
-    plt.title(f'ns {ns}')
+    plt.title(f'frame {frame}')
     pk.plot_qring(0.4)
-
-
 
 
 
